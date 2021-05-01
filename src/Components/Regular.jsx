56a4a7f952ddaa54413b68a48e8233e48e152d5f
chimny <aleks713@gmail.com>
2021-05-01 10:46:46 +0200

    working on object update within updVoteHandlerfunction

--- src/Components/Regular.jsx
+++ src/Components/Regular.jsx
@@ -1,53 +1,59 @@
 import {useState} from "react";
 import {Mem} from "./Mem";
 
-const memeTitleRegular = [
-    {
-        name: "mem1",
-        upVote: 0,
-        downVote: 0,
-        id: "A",
-    },
-    {
-        name: "mem2",
-        upVote: 0,
-        downVote: 0,
-        id: "B",
-    },
-    {
-        name: "mem3",
-        upVote: 0,
-        downVote: 0,
-        id: "C",
-    },
-    {
-        name: "mem4",
-        upVote: 0,
-        downVote: 0,
-        id: "D",
-    },
-];
-
 // odpala się od razu funkcja, do sprawdzenia
 const upVoteHandler = (array, index) => {
     return console.log(array[index].upVote);
 };
 
-const memDisplayfunction = (array) => {
-    return array.map((element, index) => (
-        <Mem
-            title={element.name}
-            upVote={element.upVote}
-            downVote={element.downVote}
-            upVoteHandler={upVoteHandler}
-            key={element.id}
-            index={index}
-            array={memeTitleRegular}
-        />
-    ));
-};
-
 export const Regular = () => {
+    const [memeTitleRegular, setMemeTitleRegular] = useState([
+        {
+            name: "mem1",
+            upVote: 0,
+            downVote: 0,
+            id: "A",
+        },
+        {
+            name: "mem2",
+            upVote: 0,
+            downVote: 0,
+            id: "B",
+        },
+        {
+            name: "mem3",
+            upVote: 0,
+            downVote: 0,
+            id: "C",
+        },
+        {
+            name: "mem4",
+            upVote: 0,
+            downVote: 0,
+            id: "D",
+        },
+    ]);
+
+    // odpala się od razu funkcja, do sprawdzenia
+    const upVoteHandler = (array, index) => {
+        setMemeTitleRegular((prevState) => {
+            return {...prevState};
+        });
+    };
+
+    const memDisplayfunction = (array) => {
+        return array.map((element, index) => (
+            <Mem
+                title={element.name}
+                upVote={element.upVote}
+                downVote={element.downVote}
+                upVoteHandler={upVoteHandler}
+                key={element.id}
+                index={index}
+                array={memeTitleRegular}
+            />
+        ));
+    };
     // // tbc
     // const [mems, setMems] = useState(null);
     return <>{memDisplayfunction(memeTitleRegular)}</>;
