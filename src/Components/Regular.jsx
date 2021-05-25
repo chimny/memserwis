c91175279f8cc380e683c31b1bf59a731fa21b25
chimny <aleks713@gmail.com>
2021-05-25 23:45:39 +0200

    problem solved

--- src/Components/Regular.jsx
+++ src/Components/Regular.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import {memDisplayfunction} from "../Functions/functions";
-import {AppContext} from "../Context/appContext";
+import {AppContext} from "../Context/AppContext";
 
 export const Regular = () => {
     const {memeRegular, setMemeRegular} = useContext(AppContext);
