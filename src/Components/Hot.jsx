c040f4b9147ac87bb202c8d1fb51f09743639a3a
chimny <aleks713@gmail.com>
2021-06-08 23:12:15 +0200

    deleted function folder

--- src/Components/Hot.jsx
+++ src/Components/Hot.jsx
@@ -1,12 +1,10 @@
 import React, {useContext} from "react";
-
 import {AppContext} from "../Context/AppContext";
 import {Mem} from "./Mem";
 
 export const Hot = () => {
-    const {memes, setMemes, topMemes} = useContext(AppContext);
+    const {topMemes} = useContext(AppContext);
 
-    // zmodyfikować propsy
     return (
         <>
             {topMemes.map((mem) => (
