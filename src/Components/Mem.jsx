0a97d070153de0fe0af6f0578e090b902b91ba62
chimny <aleks713@gmail.com>
2021-06-08 23:09:10 +0200

    solved upVote and downVote issue

--- src/Components/Mem.jsx
+++ src/Components/Mem.jsx
@@ -1,15 +1,22 @@
 import {MemWrapper} from "../Styles/Style";
+import React, {useContext} from "react";
+import {AppContext} from "../Context/AppContext";
 
 export const Mem = (props) => {
-    const {
-        title,
-        upVote,
-        downVote,
-        downVoteHandler,
-        upVoteHandler,
-        imgSRC,
-        id,
-    } = props;
+    const {memes, setMemes} = useContext(AppContext);
+    const {title, upVote, downVote, imgSRC, id} = props;
+
+    const upVoteHandler = (id) => {
+        const initialState = [...memes];
+        const updatedMem = initialState.find((mem) => mem.id === id);
+        setMemes([updatedMem.upVote++, ...initialState]);
+    };
+
+    const downVoteHandler = (id) => {
+        const initialState = [...memes];
+        const updatedMem = initialState.find((mem) => mem.id === id);
+        setMemes([updatedMem.downVote--, ...initialState]);
+    };
 
     return (
         <MemWrapper>
@@ -20,7 +27,6 @@ export const Mem = (props) => {
                 Liczba downVote : {downVote}
             </div>
             <div>
-                {/* zastąpić index -> ID */}
                 <button onClick={() => upVoteHandler(id)}>UpVote</button>
                 <button onClick={() => downVoteHandler(id)}>DownVote</button>
             </div>
