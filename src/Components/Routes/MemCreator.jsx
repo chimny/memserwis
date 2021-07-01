5977d2b2b65f47adcc60550d9414c608696a71c8
chimny <aleks713@gmail.com>
2021-07-01 15:37:22 +0200

    added useState within form in MemCreator component

--- src/Components/Routes/MemCreator.jsx
+++ src/Components/Routes/MemCreator.jsx
@@ -1,10 +1,14 @@
 import uuid from "react-uuid";
-import React, {useContext} from "react";
+import React, {useState, useContext} from "react";
 import {AppContext} from "../../Context/AppContext";
-import { StyledRoute } from "./StyledRoute";
+import {StyledRoute} from "./StyledRoute";
 
 export const MemCreator = () => {
     const {setMemes} = useContext(AppContext);
+    const [form, setForm] = useState({
+        title: "Loki",
+        imgSRC: "https://gfx.radiozet.pl/var/radiozetsg/storage/images/seriale/loki-jest-postacia-lgbt-nowa-zapowiedz-serialu-marvela/16348844-1-pol-PL/Nowa-zapowiedz-serialu-Marvela-potwierdza-ze-Loki-jest-postacia-LGBT-WIDEO_article.jpg",
+    });
 
     const NewMemHandler = (textValue, imgValue) => {
         const NewMem = {
@@ -15,7 +19,6 @@ export const MemCreator = () => {
             imgSRC: imgValue,
             star: false,
         };
-
         setMemes((prevState) => [NewMem, ...prevState]);
         alert("dodano nowy mem");
     };
@@ -30,11 +33,17 @@ export const MemCreator = () => {
     return (
         <StyledRoute>
             <form onSubmit={createMemHandler}>
-                <input type="text" placeholder="Title" id="title" />
+                <input
+                    type="text"
+                    placeholder="Title"
+                    id="title"
+                    value={form.title}
+                />
                 <input
                     type="text"
                     placeholder="IMG SRC (enter URL)"
                     id="imgSrc"
+                    value={form.imgSRC}
                 />
                 <button>Add new mem</button>
             </form>
