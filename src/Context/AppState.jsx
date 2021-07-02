2034d4955696f5fcdc147ebc56fb396bfc961929
chimny <aleks713@gmail.com>
2021-07-02 11:31:39 +0200

    implementing code review feedback

--- src/Context/AppState.jsx
+++ src/Context/AppState.jsx
@@ -2,41 +2,43 @@ import {useState} from "react";
 import {AppContext} from "./AppContext";
 import uuid from "react-uuid";
 
+const initialMemes = [
+    {
+        name: "mem1",
+        upVote: 0,
+        downVote: 0,
+        id: `${uuid()}`,
+        imgSRC: "https://i1.kwejk.pl/k/obrazki/2021/05/t7OC4YmUU65nB9Rs.jpg",
+        star: true,
+    },
+    {
+        name: "mem2",
+        upVote: 7,
+        downVote: 0,
+        id: "B",
+        imgSRC: "https://i1.kwejk.pl/k/obrazki/2021/05/1qEHO1byIHfoThhb.jpg",
+        star: false,
+    },
+    {
+        name: "mem3",
+        upVote: 0,
+        downVote: 0,
+        id: `${uuid()}`,
+        imgSRC: "https://i1.kwejk.pl/k/obrazki/2021/05/tzgTaBd1Kvs6aMh7.jpg",
+        star: true,
+    },
+    {
+        name: "mem4",
+        upVote: 0,
+        downVote: 0,
+        id: `${uuid()}`,
+        imgSRC: "https://i1.kwejk.pl/k/obrazki/2021/05/8AQqXgqpZ6PVrzDi.jpg",
+        star: true,
+    },
+];
+
 export const AppState = (props) => {
-    const [memes, setMemes] = useState([
-        {
-            name: "mem1",
-            upVote: 0,
-            downVote: 0,
-            id: `${uuid()}`,
-            imgSRC: "https://i1.kwejk.pl/k/obrazki/2021/05/t7OC4YmUU65nB9Rs.jpg",
-            star: true,
-        },
-        {
-            name: "mem2",
-            upVote: 7,
-            downVote: 0,
-            id: "B",
-            imgSRC: "https://i1.kwejk.pl/k/obrazki/2021/05/1qEHO1byIHfoThhb.jpg",
-            star: false,
-        },
-        {
-            name: "mem3",
-            upVote: 0,
-            downVote: 0,
-            id: `${uuid()}`,
-            imgSRC: "https://i1.kwejk.pl/k/obrazki/2021/05/tzgTaBd1Kvs6aMh7.jpg",
-            star: true,
-        },
-        {
-            name: "mem4",
-            upVote: 0,
-            downVote: 0,
-            id: `${uuid()}`,
-            imgSRC: "https://i1.kwejk.pl/k/obrazki/2021/05/8AQqXgqpZ6PVrzDi.jpg",
-            star: true,
-        },
-    ]);
+    const [memes, setMemes] = useState(initialMemes);
 
     const [form, setForm] = useState({
         title: "Loki",
