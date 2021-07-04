a39de2702146f55b3744e42f237f96d4e2a17251
chimny <aleks713@gmail.com>
2021-07-04 13:40:36 +0200

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,60 +1,62 @@
 import * as React from "react";
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+    HashRouter,
 } from "react-router-dom";
 // Routes
-import { Hot } from "./Components/Routes/Hot";
-import { Regular } from "./Components/Routes/Regular";
-import { Star } from "./Components/Routes/Star";
-import { MemCreator } from "./Components/Routes/MemCreator";
-import { PageNotFound } from "./Components/Routes/PageNotFound";
+import {Hot} from "./Components/Routes/Hot";
+import {Regular} from "./Components/Routes/Regular";
+import {Star} from "./Components/Routes/Star";
+import {MemCreator} from "./Components/Routes/MemCreator";
+import {PageNotFound} from "./Components/Routes/PageNotFound";
 // State
-import { AppState } from "./Context/AppState";
+import {AppState} from "./Context/AppState";
 // Styles
-import { GlobalStyle } from "./GlobalStyles/GlobalStyle";
-import { Navigation } from "./Components/Navigation/Navigation";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./GlobalStyles/theme";
+import {GlobalStyle} from "./GlobalStyles/GlobalStyle";
+import {Navigation} from "./Components/Navigation/Navigation";
+import {ThemeProvider} from "styled-components";
+import {theme} from "./GlobalStyles/theme";
 
 function App() {
-  return (
-    <div className="App">
-      <AppState>
-        <ThemeProvider theme={theme}>
-          <Router>
-            <Navigation />
-            <div>
-              <Switch>
-                <Route path="/regular">
-                  <Regular />
-                </Route>
-                <Route path="/hot">
-                  <Hot />
-                </Route>
-                <Route path="/star">
-                  <Star />
-                </Route>
-                <Route exact path="/">
-                  <Redirect to="/regular" />
-                </Route>
-                <Route path="/memCreator">
-                  <MemCreator />
-                </Route>
-                <Route path="*">
-                  <PageNotFound />
-                </Route>
-              </Switch>
-            </div>
-          </Router>
-          <GlobalStyle />
-        </ThemeProvider>
-      </AppState>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppState>
+                <ThemeProvider theme={theme}>
+                    <HashRouter>
+                        <Navigation />
+                        <div>
+                            <Switch>
+                                <Route path="/regular">
+                                    <Regular />
+                                </Route>
+                                <Route path="/hot">
+                                    <Hot />
+                                </Route>
+                                <Route path="/star">
+                                    <Star />
+                                </Route>
+                                <Route exact path="/">
+                                    <Redirect to="/regular" />
+                                </Route>
+                                <Route path="/memCreator">
+                                    <MemCreator />
+                                </Route>
+                                <Route path="*">
+                                    <PageNotFound />
+                                </Route>
+                            </Switch>
+                        </div>
+                    </HashRouter>
+
+                    <GlobalStyle />
+                </ThemeProvider>
+            </AppState>
+        </div>
+    );
 }
 
 export default App;
